a877193b71204e50ed2998742ff37d133cf1de8c
Bruno van der Kraan <brunovdkraan@gmail.com>
2016-04-19 11:50:23 +0200

    Added realistic values to all seeds to reach their targets. Added "WeightTarget" seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,112 +1,123 @@
   # This file should contain all the record creation needed to seed the database with its default values.
   # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-  #
-  # Examples:
-  #
-  #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-  #   Mayor.create(name: 'Emanuel', city: cities.first)
-  #zicht 
-  Sight.create(client_id: "1", activity_date: "2015-01-02 10:30:00", sight_left: "-2", sight_right: "-3", cilinder_left: "0.25", cilinder_right: "0.50", 
-    comments: "Nieuwe glazen voor haar bril aangemeten", reason: "Mevrouw heeft last van haar ogen en klaagt over een verminderd zicht", )
-  Sight.create(client_id: "1", activity_date: "2015-06-02 10:30:00", sight_left: "-2", sight_right: "-3", cilinder_left: "0.25", cilinder_right: "0.50", 
-    comments: "Nieuwe glazen voor haar bril aangemeten", reason: "Mevrouw heeft last van haar ogen en klaagt over een verminderd zicht", )
-  Sight.create(client_id: "1", activity_date: "2015-09-02 10:30:00", sight_left: "-2", sight_right: "-3", cilinder_left: "0.25", cilinder_right: "0.50", 
-    comments: "Nieuwe glazen voor haar bril aangemeten", reason: "Mevrouw heeft last van haar ogen en klaagt over een verminderd zicht", )
+  
+  #sight 
+  Sight.create(client_id: '1', activity_date: '2015-01-02 10:30:00', sight_left: '-3', sight_right: '-2,75', cilinder_left: '0,25', cilinder_right: '0.50', 
+    comments: 'Nieuwe glazen voor haar bril aangemeten', reason: 'Mevrouw heeft last van haar ogen en klaagt over een verminderd zicht', )
+  Sight.create(client_id: '1', activity_date: '2015-06-02 10:30:00', sight_left: '-4', sight_right: '-3,25', cilinder_left: '0,50', cilinder_right: '0.50', 
+    comments: 'Nieuwe glazen voor haar bril aangemeten', reason: 'Mevrouw heeft last van haar ogen en klaagt over een verminderd zicht', )
+  Sight.create(client_id: '1', activity_date: '2015-09-02 10:30:00', sight_left: '-4,50', sight_right: '-3,75', cilinder_left: '1,0', cilinder_right: '0.50', 
+    comments: 'Nieuwe glazen voor haar bril aangemeten', reason: 'Mevrouw heeft last van haar ogen en klaagt over een verminderd zicht', )
 
   #client 
-  Client.create(name: "Chris", surname: "Lient", 
-  photo_url: "http://www.google.nl/photos/chris_lient.jpg", 
-  date_of_birth: "1947-01-02", 
-  blood_type: "0", room: "D18", institution: "1")
+  Client.create(name: 'Chris', surname: 'Lient', 
+  photo_url: 'http://www.google.nl/photos/chris_lient.jpg', 
+  date_of_birth: '1947-01-02', 
+  blood_type: '0', room: 'D18', institution: '1')
 
   #heartrate
-  HeartRate.create(client_id: "1", activity_date: "2015-01-01 10:30:00", pulse: "76")
-  HeartRate.create(client_id: "1", activity_date: "2015-04-01 10:30:00", pulse: "68")
-  HeartRate.create(client_id: "1", activity_date: "2015-07-01 10:30:00", pulse: "69")
-  HeartRate.create(client_id: "1", activity_date: "2015-10-01 10:30:00", pulse: "63")
-  HeartRate.create(client_id: "1", activity_date: "2016-01-01 10:30:00", pulse: "66")
-  HeartRateTarget.create(client_id: "1" pulse: "68")
+  HeartRate.create(client_id: '1', activity_date: '2015-01-01 10:30:00', pulse: '76')
+  HeartRate.create(client_id: '1', activity_date: '2015-04-01 10:30:00', pulse: '68')
+  HeartRate.create(client_id: '1', activity_date: '2015-07-01 10:30:00', pulse: '69')
+  HeartRate.create(client_id: '1', activity_date: '2015-10-01 10:30:00', pulse: '63')
+  HeartRate.create(client_id: '1', activity_date: '2016-01-01 10:30:00', pulse: '66')
+  HeartRateTarget.create(client_id: '1' pulse: '68')
 
+  #bloodpressure
+  Bloodpressure.create(client_id: '1', sys: '160', dia: '90', activity_date: '2015-01-01')
+  Bloodpressure.create(client_id: '1', sys: '155', dia: '88', activity_date: '2015-04-01')
+  Bloodpressure.create(client_id: '1', sys: '130', dia: '80', activity_date: '2015-07-01')
+  Bloodpressure.create(client_id: '1', sys: '150', dia: '92', activity_date: '2015-10-01')
+  Bloodpressure.create(client_id: '1', sys: '165', dia: '94', activity_date: '2016-01-01')
+  BloodpressureTarget.create(sys: '110', dia: '70')
 
   #allergies
-  Allergy.create(name: "Noten allergie")
-	Allergy.create(name: "Lactose intolerantie")
-
+  Allergy.create(name: 'Noten allergie')
+  Allergy.create(name: 'Lactose intolerantie')
 
   #weight
-  Weight.create(client_id: "1", activity_date: "2015-01-02 10:21:00", weight: "82kg")
-  Weight.create(client_id: "1", activity_date: "2015-06-02 10:40:00", weight: "81kg")
-  Weight.create(client_id: "1", activity_date: "2015-09-02 10:32:00", weight: "86kg")
+  Weight.create(client_id: '1', activity_date: '2015-01-02 10:21:00', weight: '82kg')
+  Weight.create(client_id: '1', activity_date: '2015-06-02 10:40:00', weight: '81kg')
+  Weight.create(client_id: '1', activity_date: '2015-09-02 10:32:00', weight: '86kg')
+  WeightTarget.create(client_id: '1', weight: '66 kg', date: '2016-01-01')
 
   #length 
-  Length.create(client_id: "1", activity_date: "2015-02-02 10:22:00", length: "180cm")
-  Length.create(client_id: "1", activity_date: "2015-06-02 10:40:00", length: "179cm")
-  Length.create(client_id: "1", activity_date: "2015-09-02 10:32:00", length: "178cm")
-
-
+  Length.create(client_id: '1', activity_date: '2015-01-02 10:22:00', length: '167cm')
+  Length.create(client_id: '1', activity_date: '2015-06-02 10:40:00', length: '167cm')
+  Length.create(client_id: '1', activity_date: '2015-09-02 10:32:00', length: '166cm')
 
   #medication
-  Medication.create(name: "Tysilopine", quantity: "2mg")
+  Medication.create(name: 'Tysilopine', quantity: '2mg')
 
   #institution
-  Institution.create(city: "Leeuwarden", adress: "Beukenlaan 21", zip: "7291ED", name: "Tepias") 
-  #De diabetes patient behaald zijn/haar streefwaarden
-  #De diabetes patient behaald zijn/haar streefwaarden
+  Institution.create(city: 'Leeuwarden', adress: 'Beukenlaan 21', zip: '7291ED', name: 'Tepias') 
+
   #HB1AC
-  Hba1c.create(activity_date: "2015-01-01", mmol: "53")
-  Hba1c.create(activity_date: "2015-04-01", mmol: "58")
-  Hba1c.create(activity_date: "2015-07-01", mmol: "47")
-  Hba1c.create(activity_date: "2015-10-01", mmol: "53")
-  Hba1c.create(activity_date: "2016-01-01", mmol: "53")
-  Hba1cTarget.create(client_id: "1", mmol: "53")
-  #De diabetes patient ondervindt te veel hypers
-  #De diabetes patient heeft geen hulp gekregen bij een hyper
-  #Glucose
-  Glucose.create(client_id: "1", mmol: "5.2", activity_date: "2016-01-01 07:00:00")
-  Glucose.create(client_id: "1", mmol: "10.0", activity_date: "2016-01-01 19:00:00")
-  Glucose.create(client_id: "1", mmol: "8.0", activity_date: "2016-01-01 20:00:00")
-  Glucose.create(client_id: "1", mmol: "7.6", activity_date: "2016-01-01 22:00:00")
-  Glucose.create(client_id: "1", mmol: "5.3", activity_date: "2016-01-02 07:00:00")
-  Glucose.create(client_id: "1", mmol: "10.0", activity_date: "2016-01-02 19:00:00")
-  Glucose.create(client_id: "1", mmol: "6.3,", activity_date: "2016-01-02 20:00:00")
-  Glucose.create(client_id: "1", mmol: "5.8", activity_date: "2016-01-02 22:00:00")
-  Glucose.create(client_id: "1", mmol: "4.0", activity_date: "2016-01-03 07:00:00")
-  Glucose.create(client_id: "1", mmol: "4.9", activity_date: "2016-01-03 19:00:00")
-  Glucose.create(client_id: "1", mmol: "5.8", activity_date: "2016-01-03 22:00:00")
-  Glucose.create(client_id: "1", mmol: "4.3", activity_date: "2016-01-04 07:00:00")
-  Glucose.create(client_id: "1", mmol: "8.1", activity_date: "2016-01-04 12:00:00")
-  Glucose.create(client_id: "1", mmol: "10.9", activity_date: "2016-01-04 19:00:00")
-  Glucose.create(client_id: "1", mmol: "5.8", activity_date: "2016-01-04 22:00:00")
-  Glucose.create(client_id: "1", mmol: "5.3", activity_date: "2016-01-05 07:00:00")
-  Glucose.create(client_id: "1", mmol: "10.9", activity_date: "2016-01-05 12:00:00")
-  Glucose.create(client_id: "1", mmol: "8.5", activity_date: "2016-01-05 13:00:00")
-  Glucose.create(client_id: "1", mmol: "8.0", activity_date: "2016-01-05 19:00:00")
-  Glucose.create(client_id: "1", mmol: "5.8", activity_date: "2016-01-05 22:00:00")
-  Glucose.create(client_id: "1", mmol: "5.3", activity_date: "2016-01-06 07:00:00")
-  Glucose.create(client_id: "1", mmol: "10.9", activity_date: "2016-01-06 12:00:00")
-  Glucose.create(client_id: "1", mmol: "8.0", activity_date: "2016-01-06 19:00:00")
-  Glucose.create(client_id: "1", mmol: "5.8", activity_date: "2016-01-06 22:00:00")
-  Glucose.create(client_id: "1", mmol: "5.3", activity_date: "2016-01-07 07:00:00")
-  Glucose.create(client_id: "1", mmol: "8.0", activity_date: "2016-01-07 19:00:00")
-  Glucose.create(client_id: "1", mmol: "5.8", activity_date: "2016-01-07 22:00:00")
-  Glucose.create(client_id: "1", mmol: "5.3", activity_date: "2016-01-08 07:00:00")
-  Glucose.create(client_id: "1", mmol: "10.9", activity_date: "2016-01-08 12:00:00")
-  Glucose.create(client_id: "1", mmol: "8.0", activity_date: "2016-01-08 19:00:00")
-  Glucose.create(client_id: "1", mmol: "5.8", activity_date: "2016-01-08 22:00:00")
-  GlucoseTarget.create(client_id: "1", low: "4.0", high: "8.0")
-  ActivityType.create(name: "medicatie")
-  ActivityType.create(name: "beweging")
-  ActivityType.create(name: "voeding")
+  Hba1c.create(activity_date: '2015-06-01', mmol: '64')
+  Hba1c.create(activity_date: '2016-01-01', mmol: '75')
+  Hba1cTarget.create(client_id: '1', mmol: '48')
+  
+  #glucose
+  Glucose.create(client_id: '1', mmol: '5.2', activity_date: '2016-01-01 07:00:00')
+  Glucose.create(client_id: '1', mmol: '10.0', activity_date: '2016-01-01 19:00:00')
+  Glucose.create(client_id: '1', mmol: '8.0', activity_date: '2016-01-01 20:00:00')
+  Glucose.create(client_id: '1', mmol: '7.6', activity_date: '2016-01-01 22:00:00')
+  Glucose.create(client_id: '1', mmol: '5.3', activity_date: '2016-01-02 07:00:00')
+  Glucose.create(client_id: '1', mmol: '10.0', activity_date: '2016-01-02 19:00:00')
+  Glucose.create(client_id: '1', mmol: '6.3,', activity_date: '2016-01-02 20:00:00')
+  Glucose.create(client_id: '1', mmol: '5.8', activity_date: '2016-01-02 22:00:00')
+  Glucose.create(client_id: '1', mmol: '4.0', activity_date: '2016-01-03 07:00:00')
+  Glucose.create(client_id: '1', mmol: '4.9', activity_date: '2016-01-03 19:00:00')
+  Glucose.create(client_id: '1', mmol: '5.8', activity_date: '2016-01-03 22:00:00')
+  Glucose.create(client_id: '1', mmol: '4.3', activity_date: '2016-01-04 07:00:00')
+  Glucose.create(client_id: '1', mmol: '8.1', activity_date: '2016-01-04 12:00:00')
+  Glucose.create(client_id: '1', mmol: '10.9', activity_date: '2016-01-04 19:00:00')
+  Glucose.create(client_id: '1', mmol: '5.8', activity_date: '2016-01-04 22:00:00')
+  Glucose.create(client_id: '1', mmol: '5.3', activity_date: '2016-01-05 07:00:00')
+  Glucose.create(client_id: '1', mmol: '10.9', activity_date: '2016-01-05 12:00:00')
+  Glucose.create(client_id: '1', mmol: '8.5', activity_date: '2016-01-05 13:00:00')
+  Glucose.create(client_id: '1', mmol: '8.0', activity_date: '2016-01-05 19:00:00')
+  Glucose.create(client_id: '1', mmol: '5.8', activity_date: '2016-01-05 22:00:00')
+  Glucose.create(client_id: '1', mmol: '5.3', activity_date: '2016-01-06 07:00:00')
+  Glucose.create(client_id: '1', mmol: '10.9', activity_date: '2016-01-06 12:00:00')
+  Glucose.create(client_id: '1', mmol: '8.0', activity_date: '2016-01-06 19:00:00')
+  Glucose.create(client_id: '1', mmol: '5.8', activity_date: '2016-01-06 22:00:00')
+  Glucose.create(client_id: '1', mmol: '5.3', activity_date: '2016-01-07 07:00:00')
+  Glucose.create(client_id: '1', mmol: '8.0', activity_date: '2016-01-07 19:00:00')
+  Glucose.create(client_id: '1', mmol: '5.8', activity_date: '2016-01-07 22:00:00')
+  Glucose.create(client_id: '1', mmol: '5.3', activity_date: '2016-01-08 07:00:00')
+  Glucose.create(client_id: '1', mmol: '10.9', activity_date: '2016-01-08 12:00:00')
+  Glucose.create(client_id: '1', mmol: '8.0', activity_date: '2016-01-08 19:00:00')
+  Glucose.create(client_id: '1', mmol: '5.8', activity_date: '2016-01-08 22:00:00')
+  GlucoseTarget.create(client_id: '1', low: '4.0', high: '8.0')
+  
+  ActivityType.create(name: 'medicatie')
+  ActivityType.create(name: 'beweging')
+  ActivityType.create(name: 'voeding')
 
   #Log activity type start_time end_time comments client_id
-  Log.create(client_id: "1", description: "Insuline ingespoten", activity_type_id: "1", start_time: "2016-01-01 19:10:00", end_time: "2016-01-01 19:30:00")
-  Log.create(client_id: "1", description: "Zwemmen", activity_type_id: "2", start_time: "2016-01-01 20:00:00", end_time: "2016-01-01 21:00:00")
-  Log.create(client_id: "1", description: "Oliebollen gegeten", activity_type_id: "3", start_time: "2016-01-02 17:00:00", end_time: "2016-01-02 17:30:00")
-  Log.create(client_id: "1", description: "Insuline ingespoten", activity_type_id: "1", start_time: "2016-01-02 19:10:00", end_time: "2016-01-02 19:30:00")
-  Log.create(client_id: "1", description: "Insuline ingespoten", activity_type_id: "1", start_time: "2016-01-04 19:10:00", end_time: "2016-01-04 19:20:00")
-  Log.create(client_id: "1", description: "Appeltaart van kees", activity_type_id: "3", start_time: "2016-01-05 10:10:00", end_time: "2016-01-05 10:20:00")
-  Log.create(client_id: "1", description: "Insuline ingespoten", activity_type_id: "1", start_time: "2016-01-05 12:10:00", end_time: "2016-01-05 12:20:00")
-  Log.create(client_id: "1", description: "Appeltaart", activity_type_id: "3", start_time: "2016-01-06 09:10:00", end_time: "2016-01-06 10:00:00")
-  Log.create(client_id: "1", description: "Insuline ingespoten", activity_type_id: "1", start_time: "2016-01-06 12:10:00", end_time: "2016-01-06 12:20:00")
-  Log.create(client_id: "1", description: "Appeltaart", activity_type_id: "3", start_time: "2016-01-08 10:10:00", end_time: "2016-01-08 10:30:00")
-  Log.create(client_id: "1", description: "Insuline gehad", activity_type_id: "1", start_time: "2016-01-08 12:10:00", end_time: "2016-01-08 12:20:00")
+  #week 1
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-01 19:10:00', end_time: '2016-01-01 19:30:00')
+  Log.create(client_id: '1', description: 'Zwemmen', activity_type_id: '2', start_time: '2016-01-01 20:00:00', end_time: '2016-01-01 21:00:00')
+  Log.create(client_id: '1', description: 'Oliebollen gegeten', activity_type_id: '3', start_time: '2016-01-02 17:00:00', end_time: '2016-01-02 17:30:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-02 19:10:00', end_time: '2016-01-02 19:30:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-04 19:10:00', end_time: '2016-01-04 19:20:00')
+  Log.create(client_id: '1', description: 'Appeltaart van kees', activity_type_id: '3', start_time: '2016-01-05 10:10:00', end_time: '2016-01-05 10:20:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-05 12:10:00', end_time: '2016-01-05 12:20:00')
+  Log.create(client_id: '1', description: 'Appeltaart', activity_type_id: '3', start_time: '2016-01-06 09:10:00', end_time: '2016-01-06 10:00:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-06 12:10:00', end_time: '2016-01-06 12:20:00')
+  Log.create(client_id: '1', description: 'Appeltaart', activity_type_id: '3', start_time: '2016-01-08 10:10:00', end_time: '2016-01-08 10:30:00')
+  Log.create(client_id: '1', description: 'Insuline gehad', activity_type_id: '1', start_time: '2016-01-08 12:10:00', end_time: '2016-01-08 12:20:00')
+
+  #week2 
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-08 19:10:00', end_time: '2016-01-08 19:30:00')
+  Log.create(client_id: '1', description: 'Zwemmen', activity_type_id: '2', start_time: '2016-01-09 20:00:00', end_time: '2016-01-09 21:00:00')
+  Log.create(client_id: '1', description: 'Oliebollen gegeten', activity_type_id: '3', start_time: '2016-01-10 17:00:00', end_time: '2016-01-10 17:30:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-11 19:10:00', end_time: '2016-01-11 19:30:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-12 19:10:00', end_time: '2016-01-12 19:20:00')
+  Log.create(client_id: '1', description: 'Appeltaart van kees', activity_type_id: '3', start_time: '2016-01-13 10:10:00', end_time: '2016-01-13 10:20:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-14 12:10:00', end_time: '2016-01-14 12:20:00')
+  Log.create(client_id: '1', description: 'Appeltaart', activity_type_id: '3', start_time: '2016-01-15 09:10:00', end_time: '2016-01-15 10:00:00')
+  Log.create(client_id: '1', description: 'Insuline ingespoten', activity_type_id: '1', start_time: '2016-01-16 12:10:00', end_time: '2016-01-16 12:20:00')
+  Log.create(client_id: '1', description: 'Appeltaart', activity_type_id: '3', start_time: '2016-01-17 10:10:00', end_time: '2016-01-17 10:30:00')
+  Log.create(client_id: '1', description: 'Insuline gehad', activity_type_id: '1', start_time: '2016-01-18 12:10:00', end_time: '2016-01-18 12:20:00')
