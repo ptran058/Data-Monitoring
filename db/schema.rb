d99eaa38646ae7652d30343aaae2402ea7f00b33
Bruno van der Kraan <brunovdkraan@gmail.com>
2016-04-21 11:08:44 +0200

    Model update

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20160420130957) do
 
   create_table "activity_types", force: :cascade do |t|
