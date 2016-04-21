2d1fb2f5324b0549286fe9fc2821ff42c783136d
Bruno van der Kraan <brunovdkraan@gmail.com>
2016-04-21 09:58:17 +0200

    Deleted sample text

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
   # This file should contain all the record creation needed to seed the database with its default values.
   # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-  #
-  # Examples:
-  #
-  #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-  #   Mayor.create(name: 'Emanuel', city: cities.first)
   #zicht 
   Sight.create(client_id: "1", activity_date: "2015-01-02 10:30:00", sight_left: "-2", sight_right: "-3", cilinder_left: "0.25", cilinder_right: "0.50", 
     comments: "Nieuwe glazen voor haar bril aangemeten", reason: "Mevrouw heeft last van haar ogen en klaagt over een verminderd zicht", )
