7b775eac3b19d1ceeabb2d52b509100c2ea54d84
ptran058 <ptran058@gmail.com>
2016-04-22 05:54:58 -0700

    Added Functions: Add client Show client Client overview

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -10,4 +10,20 @@ class ClientsController < ApplicationController
     @sight = Sight.all
 
   end
+  def new
+    @client = Client.new
+  end
+
+  def create
+    @client = Client.new (client_params)
+    @client.save 
+    redirect_to clients_path
+  end
+  def show
+    @client = Client.find(params[:id])
+  end
+  private
+    def client_params
+      params.require(:client).permit(:photo_url, :name, :surname, :date_of_birth, :room, :institution, :blood_type)
+    end
 end
