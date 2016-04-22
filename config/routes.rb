60a362ccc6bc4dcaeb917cdbc0b62050409039f1
Jorik Leppink <jorikleppink@gmail.com>
2016-04-22 09:42:33 +0200

    Added cholesterols controller and cholesterol form

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   resources :clients
   resources :sights
   resources :heartrates
+  resources :cholesterols
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
