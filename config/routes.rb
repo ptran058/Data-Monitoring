57d00f106265806f4265cf4b538391c72b3de89a
ptran058 <ptran058@gmail.com>
2016-05-19 05:16:44 -0700

    Created blood pressures form

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   resources :glucoses
   resources :weights
   resources :hba1c_targets
+  resources :blood_pressure_targets
   
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
