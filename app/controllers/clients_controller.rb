9a4e5f7549fe87acedd40634934b68160e4b0a96
ptran058 <ptran058@gmail.com>
2016-05-19 03:53:41 -0700

    Added hba1c target form

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -24,6 +24,10 @@ class ClientsController < ApplicationController
     @glucose = Glucose.new
     @glucoses = Glucose.where(client_id: params[:id]).order('activity_date ASC')
     @log = Log.where(client_id: params[:id])
+    @hba1c = Hba1cTarget.where(client_id: params[:id])
+
+    @hba1c_target = Hba1cTarget.new
+
   end
   private
     def client_params
