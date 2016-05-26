81f0841de830d759171c62374e659fb25ee6f5e0
ptran058 <ptran058@gmail.com>
2016-05-26 07:56:03 -0700

    connect the weightchart to the database

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -50,6 +50,7 @@ class ClientsController < ApplicationController
     @client_glucose_target = GlucoseTarget.where(client_id: params[:id]).last
     @client_cholesterol = Cholesterol.where(client_id: params[:id]).order('activity_date ASC').limit(4)
     @client_blood_pressure = BloodPressure.where(client_id: params[:id]).order('activity_date ASC').limit(10)
+    @client_weight = Weight.where(client_id: params[:id]).order('activity_date ASC').limit(10)
 
   end
   private
