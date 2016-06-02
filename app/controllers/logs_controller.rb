b1d61937e4452f75d1dce655caffeaa8e7e7db8a
Bruno van der Kraan <brunovdkraan@gmail.com>
2016-06-02 13:41:21 +0200

    delete unnecessary code

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,49 +1,10 @@
 class LogsController < ApplicationController
-  def index
-    @blood_pressure = BloodPressure.all
-    @cholesterol = Cholesterol.all
-    @client = Client.all
-    @length = Length.all
-    @medication = Medication.all
-    @weight = Weight.all
-    @weight_target = Weight.all
-    @log = Log.all
-    @glucose = Glucose.all
-    @glucose_target = GlucoseTarget.all
-    @sight = Sight.all
-  end
 
-  def new
-    @log = Log.new
-  end
   def create
     @log = Log.new(log_params)
     @log.save 
     redirect_to client_path(log_params[:client_id])
-
   end
-
-  def show
-    @hba1c_targets = Hba1cTarget.where(client_id: params[:id])
-    @blood_pressure_targets = BloodPressureTarget.where(client_id: params[:id])
-    @heartrate_targets = HeartrateTarget.where(client_id: params[:id])
-    @cholesterol_targets = CholesterolTarget.where(client_id: params[:id])
-    @weight = Weight.new
-    @weight_target = WeightTarget.new
-    @medication = Medication.new
-    @glucose = Glucose.new
-    @glucose_target = GlucoseTarget.new
-    @hba1c = Hba1c.new
-    @hba1c_target = Hba1cTarget.new
-    @blood_pressure = BloodPressure.new
-    @blood_pressure_target = BloodPressureTarget.new
-    @heartrate = Heartrate.new
-    @heartrate_target = HeartrateTarget.new
-    @cholesterol = Cholesterol.new
-    @cholesterol_target = CholesterolTarget.new
-    @log = Log.new
-  end
-
 
   private
     def log_params
